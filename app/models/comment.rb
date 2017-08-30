2b2ceff22ee7739a7a76c6be0039f8be90c477a7
Evgenia Manolova <emanolova@gmail.com>
2017-08-30 13:00:15 +0300

    all tests pass

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,16 @@ class Comment < ApplicationRecord
   validates :author, presence: true, length: { maximum: 100 }
   validates :body, presence: true, length: { maximum: 1000 }
   validates :vote, numericality: true, inclusion: 1..5, allow_blank: true
+
+  after_save :update_flower_rating!
+
+  def update_flower_rating!
+    if flower
+      comments_with_votes = flower.comments.where.not(vote: nil)
+      flower.votes = comments_with_votes.count
+      flower.rating = flower.votes > 0 ? comments_with_votes.sum(:vote)*1.0 / flower.votes : 0
+      flower.rating = flower.rating.round(1)
+      flower.save!
+    end
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  context "#create" do
-    let (:flower) { Flower.create(title: "My flower", description: "Some desc", votes: 3, rating: "4.2") }
+  context "#save" do
+    before(:each) do
+      Comment.create!(flower: flower, author: "me", body: "tralala", vote: 2)
+      Comment.create!(flower: flower, author: "me", body: "tralala", vote: 5)
+      Comment.create!(flower: flower, author: "me", body: "tralala", vote: 5)
+    end
+
+    let (:flower) { Flower.create(title: "My flower", description: "Some desc") }
     let (:comment) { Comment.new(flower: flower, author: "me", body: "tralala") }
 
     context "when vote attribute is set" do
+
       it "increases the flower's votes value" do
         comment.vote = 3
         expect {
@@ -17,15 +24,15 @@ RSpec.describe Comment, type: :model do
         comment.vote = 3
         expect {
           comment.save!
-        }.to change { flower.rating }.to 3.9
+        }.to change { flower.reload.rating }.to 3.8
       end
     end
 
     context "when vote attribute is NOT set" do
-      it "does Not change the flower's votes and rating values" do
+      it "does NOT change the flower's votes and rating values" do
         comment.save!
         expect(flower.reload.votes).to eq(3)
-        expect(flower.reload.rating).to eq(4.2)
+        expect(flower.reload.rating).to eq(4)
       end
     end
   end
